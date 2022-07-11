1b0538b41f38379772ac347dea0e904dcf3b2b1b
ghchoi <ghchoi0427@gmail.com>
2022-07-11 22:00:37 +0900

    feat: LandingController 생성

---
+++ src/main/java/com/cgh/inflearn/controller/LandingController.java
@@ -0,0 +1,15 @@
+package com.cgh.inflearn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class LandingController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
+}
