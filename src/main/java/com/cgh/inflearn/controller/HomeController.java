ac8e92320ed3be59fac155acecfa2af29371563a
ghchoi <ghchoi0427@gmail.com>
2022-07-11 23:28:47 +0900

    fix: 회원가입 간소화

--- src/main/java/com/cgh/inflearn/controller/HomeController.java
+++ src/main/java/com/cgh/inflearn/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
@@ -21,10 +22,7 @@ public class HomeController {
     private final MemberService memberService;
 
     @GetMapping()
-    public String home(@CookieValue(name = "memberId", required = false) String userId) {
-        if (userId == null) {
-            //return "redirect:/home/login";
-        }
+    public String home() {
         return "home";
     }
 
@@ -35,34 +33,18 @@ public class HomeController {
 
     @PostMapping("/login")
     public String signIn(MemberLoginDto dto, HttpServletResponse response) {
-        if (!memberService.userNameAlreadyExists(dto.getUsername())) {
-            return "redirect:/home/signup";
-        }
-        Member member = memberService.findByName(dto.getUsername());
-        Cookie idCookie = new Cookie("memberId", member.getId().toString());
-        response.addCookie(idCookie);
-        if (!memberService.isMember(member.getId())) {
-            return "redirect:/home/login";
-        }
-        return "redirect:/home";
-    }
+        Cookie idCookie;
 
-    @GetMapping("/signup")
-    public String signUp() {
-        return "signup";
-    }
-
-    @PostMapping("/signup")
-    public String register(MemberLoginDto dto, HttpServletResponse response) {
-        Member member = new Member(dto.getUsername(), dto.getUserPassword());
-        if (memberService.userNameAlreadyExists(dto.getUsername())) {
-            return "redirect:/home/signup";
+        if (memberService.userNameAlreadyExists(dto.getUserName()) && memberService.findByName(dto.getUserName()).isPresent()) {
+            Member member = memberService.findByName(dto.getUserName()).get();
+            idCookie = new Cookie("memberId", member.getId().toString());
+        } else {
+            Member newInstance = new Member(dto.getUserName(), dto.getUserPassword());
+            memberService.signUp(newInstance);
+            idCookie = new Cookie("memberId", newInstance.getId().toString());
         }
-
-        Cookie idCookie = new Cookie("memberId", member.getId().toString());
         idCookie.setPath("/");
         response.addCookie(idCookie);
-        memberService.signUp(member);
 
         return "redirect:/home";
     }
